744b87052c064558343d75d103a0504e64093fb3
Mihail <66316437+Mihail1186@users.noreply.github.com>
2021-02-03 21:45:40 +0300

    updated portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+VANTA.GLOBE({
+   el: "#your-element-selector",
+   mouseControls: true,
+   touchControls: true,
+   gyroControls: false,
+   minHeight: 200.00,
+   minWidth: 200.00,
+   scale: 1.00,
+   scaleMobile: 1.00,
+   size: 0.7
+ })
+
